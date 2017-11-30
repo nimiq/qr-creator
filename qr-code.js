852825840d3f1f22a28fab0265e06f8c35535b34
Sven Buschbeck <svub@x900.svub.net>
2017-11-30 12:40:15 +0800

    Google closure compiler: Original Size:9.59KB gzipped (53.32KB uncompressed) Compiled Size:4.95KB gzipped (12.36KB uncompressed)

--- qr-code.js
+++ qr-code.js
@@ -2,8 +2,6 @@
 (function(vendor_qrcode) {
     'use strict';
 
-    var jq = window.jQuery;
-
     // Check if canvas is available in the browser (as Modernizr does)
     var hasCanvas = (function() {
         var elem = document.createElement('canvas');
@@ -720,13 +718,47 @@
         // qrcode.stringToBytes
         //---------------------------------------------------------------------
 
+        //rcode.stringToBytes = function(s) {
+        //   var bytes = new Array();
+        //   for (var i = 0; i < s.length; i += 1) {
+        //       var c = s.charCodeAt(i);
+        //       bytes.push(c & 0xff);
+        //   }
+        //   return bytes;
+        //;
+        // UTF-8 version
         qrcode.stringToBytes = function(s) {
-            var bytes = new Array();
-            for (var i = 0; i < s.length; i += 1) {
-                var c = s.charCodeAt(i);
-                bytes.push(c & 0xff);
+            // http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
+            function toUTF8Array(str) {
+                var utf8 = [];
+                for (var i = 0; i < str.length; i++) {
+                    var charcode = str.charCodeAt(i);
+                    if (charcode < 0x80) utf8.push(charcode);
+                    else if (charcode < 0x800) {
+                        utf8.push(0xc0 | (charcode >> 6),
+                            0x80 | (charcode & 0x3f));
+                    } else if (charcode < 0xd800 || charcode >= 0xe000) {
+                        utf8.push(0xe0 | (charcode >> 12),
+                            0x80 | ((charcode >> 6) & 0x3f),
+                            0x80 | (charcode & 0x3f));
+                    }
+                    // surrogate pair
+                    else {
+                        i++;
+                        // UTF-16 encodes 0x10000-0x10FFFF by
+                        // subtracting 0x10000 and splitting the
+                        // 20 bits of 0x0-0xFFFFF into two halves
+                        charcode = 0x10000 + (((charcode & 0x3ff) << 10) |
+                            (str.charCodeAt(i) & 0x3ff));
+                        utf8.push(0xf0 | (charcode >> 18),
+                            0x80 | ((charcode >> 12) & 0x3f),
+                            0x80 | ((charcode >> 6) & 0x3f),
+                            0x80 | (charcode & 0x3f));
+                    }
+                }
+                return utf8;
             }
-            return bytes;
+            return toUTF8Array(s);
         };
 
         //---------------------------------------------------------------------
@@ -1636,64 +1668,6 @@
     }(function() {
         return qrcode;
     }));
-    //---------------------------------------------------------------------
-    //
-    // QR Code Generator for JavaScript UTF8 Support (optional)
-    //
-    // Copyright (c) 2011 Kazuhiko Arase
-    //
-    // URL: http://www.d-project.com/
-    //
-    // Licensed under the MIT license:
-    //  http://www.opensource.org/licenses/mit-license.php
-    //
-    // The word 'QR Code' is registered trademark of
-    // DENSO WAVE INCORPORATED
-    //  http://www.denso-wave.com/qrcode/faqpatent-e.html
-    //
-    //---------------------------------------------------------------------
-
-    ! function(qrcode) {
-
-        //---------------------------------------------------------------------
-        // overwrite qrcode.stringToBytes
-        //---------------------------------------------------------------------
-
-        qrcode.stringToBytes = function(s) {
-            // http://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
-            function toUTF8Array(str) {
-                var utf8 = [];
-                for (var i = 0; i < str.length; i++) {
-                    var charcode = str.charCodeAt(i);
-                    if (charcode < 0x80) utf8.push(charcode);
-                    else if (charcode < 0x800) {
-                        utf8.push(0xc0 | (charcode >> 6),
-                            0x80 | (charcode & 0x3f));
-                    } else if (charcode < 0xd800 || charcode >= 0xe000) {
-                        utf8.push(0xe0 | (charcode >> 12),
-                            0x80 | ((charcode >> 6) & 0x3f),
-                            0x80 | (charcode & 0x3f));
-                    }
-                    // surrogate pair
-                    else {
-                        i++;
-                        // UTF-16 encodes 0x10000-0x10FFFF by
-                        // subtracting 0x10000 and splitting the
-                        // 20 bits of 0x0-0xFFFFF into two halves
-                        charcode = 0x10000 + (((charcode & 0x3ff) << 10) |
-                            (str.charCodeAt(i) & 0x3ff));
-                        utf8.push(0xf0 | (charcode >> 18),
-                            0x80 | ((charcode >> 12) & 0x3f),
-                            0x80 | ((charcode >> 6) & 0x3f),
-                            0x80 | (charcode & 0x3f));
-                    }
-                }
-                return utf8;
-            }
-            return toUTF8Array(s);
-        };
-
-    }(qrcode);
 
     return qrcode; // eslint-disable-line no-undef
 }()));
